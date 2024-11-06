bdc717252a6d88dab4ab3a7503dc34e48c6efb40
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-06 09:10:41 +0530

    add assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Register from './pages/Register'
+import "./style.scss";
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Register/>
+    </>
   )
 }
 
