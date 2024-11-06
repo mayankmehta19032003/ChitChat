ab10d1f43c8890fac70e1fb841ea104256cb1ac9
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-06 10:42:47 +0530

    completed Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Register from './pages/Register'
 import "./style.scss";
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <>
-      <Register/>
+     
     </>
   )
 }
