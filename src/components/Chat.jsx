af08b10c7bf4c271f2d97c78a4b1d5a6723e2e38
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-07 18:45:15 +0530

    set chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Cam from "../assets/cam.png";
+import Add from "../assets/add.png";
+import More from "../assets/more.png";
 
 const Chat = () => {
   return (
     <div className='chat'>
-      chat
+      <div className="chatInfo">
+        <span>Mayank </span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
