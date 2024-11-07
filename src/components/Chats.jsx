d90283f76a2f20fb36afb09e01ae62881568de29
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-07 18:38:44 +0530

    complet sidebar

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>
-      
+    <div className='chats'>
+       <div className="userChat">
+        <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className='userChatInfo'>
+          <span>Mayank</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className='userChatInfo'>
+          <span>Mayank</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className='userChatInfo'>
+          <span>Mayank</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
     </div>
   )
 }
 
-export default Chats
+export default Chats; 
