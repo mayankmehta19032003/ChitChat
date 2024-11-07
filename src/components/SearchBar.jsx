210fbb1477bc08d5db6bd857dd58066d6f8284ef
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-07 18:32:41 +0530

    completed search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div>
-      
+    <div className='search'>
+      <div className="searchForm">
+        <input type="text" placeholder='find a user'  />
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className='userChatInfo'>
+          <span>Mayank</span>
+        </div>
+      </div>
     </div>
   )
 }
