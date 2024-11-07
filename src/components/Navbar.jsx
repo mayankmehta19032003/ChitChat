4e05eed6224a2411efc911f185b29b9e1da50712
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-07 18:09:48 +0530

    completed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <span className="logo">ChitChat</span>
+      <div className="user">
+        <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <span>Mayank</span>
+        <button>logout</button>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      Sidebar
+      <Navbar/>
     </div>
   )
 }
