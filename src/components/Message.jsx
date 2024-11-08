0457a756cf492a54752ac3975d6a81209affa6bb
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-08 06:03:07 +0530

    finish design

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      Msg
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+      </div>
     </div>
   )
 }
