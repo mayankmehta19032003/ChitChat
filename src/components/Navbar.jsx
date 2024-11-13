52b63f8a0edd0d4589eb196aaa15084be944343f
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-13 08:43:54 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   
   return (
     <div className='navbar'>
-      <span className="logo">ChitChat</span>
+      <span className="logo">ChitChat!</span>
       <div className="user">
         <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
         <span>{currentUser.displayName}</span>
